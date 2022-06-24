85a8d2a958234f5fcb33da53e5f291710dafaeff
Ebenezr <ebenezarbukosia@gmail.com>
2022-06-24 09:35:45 +0300

    fix: change timestamp to readable formart

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,146 +1,115 @@
 //const fetch = require('node-fetch');
 let timeIn;
 let timeOut;
-document.addEventListener('DOMContentLoaded',()=>{
-    fetchData()
-    let clockIn=document.getElementById('clock-in');
-    
-    
-    clockIn.addEventListener('click',()=>{
-        clockInTime()
-        
-    
-    });
-   
-})
+let usersn;
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+  let clockIn = document.getElementById("clock-in");
+
+  clockIn.addEventListener("click", () => {
+    clockInTime();
+  });
+});
 //get DOM elements
-let clockOut=document.getElementById('clock-out');
-let usersList=document.getElementById('list-results');
-let userTitle=document.getElementById('title');
-let userDesignation=document.getElementById('designation');
-let userId = document.getElementById('id');
-let timeStampList=document.getElementById('timestamp-results');
+let clockOut = document.getElementById("clock-out");
+let usersList = document.getElementById("list-results");
+let userTitle = document.getElementById("title");
+let userDesignation = document.getElementById("designation");
+let userId = document.getElementById("id");
+let timeStampList = document.getElementById("timestamp-results");
 function fetchData() {
-    fetch("http://localhost:3000/users")
-      .then((resp) => resp.json())
-      .then((users) => {
-      
-          renderUsers(users);
-          clockOutTime(users);
-          appendTimestamp(users)
+  fetch("http://localhost:3000/users")
+    .then((resp) => resp.json())
+    .then((users) => {
+      renderUsers(users);
+      clockOutTime(users);
+      appendTimestamp(users);
     });
 }
-function renderUsers(users){
-    users.forEach(users=>{
-        let list =document.createElement('li');
-        list.innerText= users.name;
-        usersList.appendChild(list);
-       
-        //render spefic movie details
-        list.addEventListener('click',()=>{
-            userTitle.textContent=users.name;
-            userDesignation.textContent=users.designation;
-            userId.textContent=users.id;
-            appendTimestamp(users)
-            
-            
+function renderUsers(users) {
+  users.forEach((users) => {
+    let list = document.createElement("li");
+    list.innerText = users.name;
+    usersList.appendChild(list);
 
+    //render spefic movie details
+    list.addEventListener("click", () => {
+      userTitle.textContent = users.name;
+      userDesignation.textContent = users.designation;
+      userId.textContent = users.id;
+      appendTimestamp(users);
+    });
+  });
+}
+function appendTimestamp(users) {
+  for (let i in users.timestamp) {
+    let li = document.createElement("li");
+    2;
+    li.innerHTML = users.timestamp[i];
+    timeStampList.appendChild(li);
+  }
+}
+
+function clockInTime() {
+  timeIn = new Date();
+  return timeIn;
+}
+function clockOutTime(users) {
+  timeOut = new Date();
+  clockOut.addEventListener("click", () => {
+    for (let data of users) {
+      if (userId.innerText === data.id) {
+        const index = users.indexOf(data);
+
+        let newtimestamp = [
+          `${timeIn.toGMTString()} - ${timeOut.toGMTString()}`,
+        ];
+        let newt = [...newtimestamp];
+        //console.log(newtimestamp);
+        const upDateInfo = {
+          id: data.id,
+          email: data.email,
+          password: data.password,
+          name: data.name,
+          designation: data.designation,
+          timestamp: [...newt],
+        };
+        //console.log(newVotes)
+
+        fetch(`http://localhost:3000/users/${index + 1}`, {
+          method: "PATCH",
+          headers: {
+            Accept: "application/json",
+            "Content-type": "application/json; charset=UTF-8",
+          },
+          body: JSON.stringify(upDateInfo),
         })
+          .then((res) => res.json())
+          .then((users) => {
+            //console.log(users)
 
-    })
-
-}
-function appendTimestamp(users){
-    for (let i in users.timestamp){
-        let li=document.createElement('li');    2
-        li.innerHTML=users.timestamp[i];
-        timeStampList.appendChild(li);
+            appendTimestamp(users);
+          })
+          .catch(function () {});
+      } else {
+      }
     }
+  });
 }
 
+const searchBar = document.querySelector("#search");
+searchBar.addEventListener("keyup", (event) => {
+  const letter = event.target.value;
+  usersList.innerHTML = "";
 
-function clockInTime(){
-    timeIn=new Date();
-    return timeIn;
+  const userListr = usersn.filter((user) => {
+    //user = users.name;
+    //console.log(user);
+    return user.startsWith(letter);
+  });
 
-}
-function clockOutTime(users){
-    timeOut=new Date();
-    clockOut.addEventListener('click',()=>{
-    for(let data of users){
-        if (userId.innerText === data.id){
-            const index = users.indexOf(users);
-            
-            let newtimestamp=`${timeIn} - ${timeOut}`
-            //console.log(newtimestamp);
-            const upDateInfo = { 
-                id: data.id,
-                email: data.email,
-                password: data.password,
-                name:data.name,
-                designation:data.designation,
-                timestamp:newtimestamp
-            };
-    //console.log(newVotes)
-    
-            fetch(`http://localhost:3000/users/${index + 1}`,{ 
-                method:'PATCH',
-                headers:{
-                    'Accept': 'application/json',
-                    'Content-type': 'application/json; charset=UTF-8'
-                },
-                body:JSON.stringify(upDateInfo)
-            })
-            .then(res => res.json())
-            .then(users => {
-                //console.log(users)
-                
-                appendTimestamp(users)
-            })
-            .catch(function (){ 
-                
-            })       
-        }else{
-
-        }
-}
-
-})
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  userListr.forEach((element) => renderUsers(element));
+});
 
 // class Timestamp{
 //     constructor(clockIn,clockOut,timeStamp){
